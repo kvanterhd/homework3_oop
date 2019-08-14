ba975ba7ed405bca9bbdbf006893972647a5f584
kvanterhd <54018863+kvanterhd@users.noreply.github.com>
2019-08-14 21:50:05 +0300

    Add files via upload

---
+++ homework13.08.2019/homework13.08.2019/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework13._08._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Human Human0_000_000_001 = new Human("Dmytro", "Timofeev", "25.01.2001");
+            Abiturient Abit0_000_001 = new Abiturient("Petro", "Petrov", "15.11.1996", 200, 198, 179, 9.78);
+            Student Stud0_000_001 = new Student("Mykola", "Bogatov", "17.02.1998", 4, "Tech33", "OOP", "Oxford");
+            Teacher Teacher0_01 = new Teacher("Ivan", "Levchenko", "13.05.1985", "Lector", "Computer Technologies", "Oxford");
+            Console.WriteLine(Abit0_000_001.GetInfo());
+            Console.WriteLine(Human0_000_000_001.GetInfo());
+            Console.WriteLine(Stud0_000_001.GetInfo());
+            Console.WriteLine(Teacher0_01.GetInfo());
+
+        }
+        }
+    }
+
---
+++ homework13.08.2019/homework13.08.2019/human.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework13._08._2019
+{
+    class Human
+    {
+        protected string FirstName;
+        protected string LastName;
+        protected string BDate;
+        public Human(string firsName,string lastName,string bDate)
+        {
+            FirstName = firsName;
+            LastName = lastName;
+            BDate = bDate;
+        }
+        public virtual string GetInfo()
+        {
+            return string.Format("First Name: {0}, Last Name: {1}, Birth date : {2}",FirstName, LastName, BDate);
+        }
+    }
+    class Abiturient : Human
+    {
+        protected int Zno1;
+        protected int Zno2;
+        protected int Zno3;
+        protected double Atestat;
+        public Abiturient(string firsName, string lastName, string bDate, int zno1, int zno2, int zno3, double atestat) :
+            base(firsName,lastName,bDate)
+        {
+            Zno1 = zno1;
+            Zno2 = zno2;
+            Zno3 = zno3;
+            Atestat = atestat;
+        }
+        public override string GetInfo()
+        {
+            return base.GetInfo() + " |||" + " ZNO1: " + Zno1 + " |||" + " ZNO2: " + Zno2 + " |||" + " ZNO3: " + Zno3 + " |||" + " Бал документа про освіту: " + Atestat + "|||";
+        }
+       
+    }
+    class Student : Human
+    {
+        protected int Course;
+        protected string Group;
+        protected string Faculty;
+        protected string Univercity;
+        public Student(string firsName, string lastName, string bDate,int course,string group,string faculty,string university) :
+            base(firsName, lastName, bDate)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            Univercity = university;
+        }
+        public override string GetInfo()
+        {
+            return base.GetInfo() + " |||" + " Course: " + Course + " |||" + " Group: " + Group + " |||" + " Faculty: " + Faculty + " |||" + " University: " + Univercity + "|||";
+        }
+    }
+    class Teacher : Human
+    {
+        protected string Post;
+        protected string Department;
+        protected string University;
+        public Teacher(string firsName, string lastName, string bDate,string post,string department,string university):
+            base(firsName, lastName, bDate)
+        {
+            Post = post;
+            Department = department;
+            University = university;
+        }
+        public override string GetInfo()
+        {
+            return base.GetInfo() + " |||" + " Post: " + Post + " |||" + " Department: " + Department + " |||" + " University: " + University + "|||";
+        }
+    }
+}
